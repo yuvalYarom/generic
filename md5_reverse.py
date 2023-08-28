33e15734be21ab1b2cf97478f270def4fe0e8c93
yuvalYarom <42272899+yuvalYarom@users.noreply.github.com>
2023-08-28 16:37:59 +0300

    Update md5_reverse.py

--- md5_reverse.py
+++ md5_reverse.py
@@ -1,6 +1,9 @@
 import urllib.request
 import ssl
 import time
+import pandas as pd
+from urllib.error import HTTPError
+
 def reverse_md5(md5_str,sleep=0):
     time.sleep(sleep)
     gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
@@ -13,3 +16,15 @@ def reverse_md5(md5_str,sleep=0):
     if start == -1 or end == -1:
         return "Provided MD5 hash could not be reversed into a string: no reverse string was found."    
     return contents[start+92:end]
+df = pd.read_csv("/Users/yyarom/Downloads/bq-results-20230828-132823-1693229330705.csv")
+for j,i in enumerate(df['ja3s']):
+    if j > -1:
+        try: 
+            t = reverse_md5(i)
+        except HTTPError:
+            print("reset")
+            time.sleep(60)
+        print(j)
+        if t != "Provided MD5 hash could not be reversed into a string: no reverse string was found.":
+            print(t)
+            res.append(t)
